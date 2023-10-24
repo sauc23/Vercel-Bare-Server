25d42db1297e5105077553d27fbbdf95503f780f
sss <89742348+sauc23@users.noreply.github.com>
2023-10-23 21:10:26 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const http = require("node:http");
+const { createBareServer } = require("@tomphttp/bare-server-node");
+const express = require("express");
+const fs = require("fs");
+
+const httpServer = http.createServer();
+const bareServer = createBareServer("/");
+
+httpServer.on("request", (req, res) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  } else {
+    res.writeHead(400);
+    res.end("Not found.");
+  }
+});
+
+httpServer.on("upgrade", (req, socket, head) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+httpServer.on("listening", () => {
+  console.log("HTTP server listening");
+});
+
+httpServer.listen({
+  port: 8000,
+});
