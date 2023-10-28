2b32fc86cd46dc0d8d26354ecf1b62a028f3c16a
sss <89742348+sauc23@users.noreply.github.com>
2023-10-28 10:40:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
 const http = require("node:http");
 const { createBareServer } = require("@tomphttp/bare-server-node");
-const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
-// Create an Express app
-const app = express();
-
-// Serve the index.html file when the root URL is requested
-app.get("/", (req, res) => {
-  const indexPath = path.join(__dirname, "index.html");
-
-  fs.readFile(indexPath, "utf8", (err, data) => {
-    if (err) {
-      res.status(500).send("Internal Server Error");
-    } else {
-      res.setHeader("Content-Type", "text/html");
-      res.status(200).send(data);
-    }
-  });
-});
-
-// Your existing HTTP server code
+// Create an HTTP server
 const httpServer = http.createServer();
 const bareServer = createBareServer("/bare/");
 
 httpServer.on("request", (req, res) => {
   if (bareServer.shouldRoute(req)) {
     bareServer.routeRequest(req, res);
+  } else if (req.url === "/") {
+    // Serve the index.html file when the root URL is requested
+    const indexPath = path.join(__dirname, "index.html");
+
+    fs.readFile(indexPath, "utf8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
   } else {
-    res.writeHead(400);
+    res.writeHead(400, { "Content-Type": "text/plain" });
     res.end("Not found.");
   }
 });
