d91476c24f90412a49b61064404965942d6805be
sss <89742348+sauc23@users.noreply.github.com>
2023-10-28 10:30:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,28 @@ const http = require("node:http");
 const { createBareServer } = require("@tomphttp/bare-server-node");
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 
+// Create an Express app
+const app = express();
+
+// Serve the index.html file when the root URL is requested
+app.get("/", (req, res) => {
+  const indexPath = path.join(__dirname, "index.html");
+
+  fs.readFile(indexPath, "utf8", (err, data) => {
+    if (err) {
+      res.status(500).send("Internal Server Error");
+    } else {
+      res.setHeader("Content-Type", "text/html");
+      res.status(200).send(data);
+    }
+  });
+});
+
+// Your existing HTTP server code
 const httpServer = http.createServer();
-const bareServer = createBareServer("/");
+const bareServer = createBareServer("/bare/");
 
 httpServer.on("request", (req, res) => {
   if (bareServer.shouldRoute(req)) {
